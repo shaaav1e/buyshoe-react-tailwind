7761f4841d3bc3ab2e47a7110d5e739586774806
shaaav1e <shaaavie@gmail.com>
2025-04-23 18:07:18 +0500

    card->Card->Vercel-fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,9 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Card from "./components/card";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Card />
